6325018728e9a2021ce922ecedc99072f81f243c
rahuldemocenter <63845942+rahuldemocenter@users.noreply.github.com>
2021-03-25 12:41:16 +0530

    Update DeployCustomerImageToK8.sh

--- DeployCustomerImageToK8.sh
+++ DeployCustomerImageToK8.sh
@@ -2,6 +2,6 @@
 #
 
 cd /opt/rancher
-./rancher login https://rancher.eur.ad.sag/v3 --token $(RANCHER_TOKEN)
+./rancher login https://rancher.eur.ad.sag/v3 --token ${RANCHER_TOKEN}
 $1
 ./rancher kubectl create -f ${WORKSPACE}/PushCustomerImageToK8.yaml 
